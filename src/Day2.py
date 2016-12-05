72ea51b2f1ed9a3da1d029b351066fd768dc3148
Andreas Weichslgartner <andreas.weichslgartner@fau.de>
2016-12-05 08:16:09 +0100

    merge

--- src/Day2.py
+++ src/Day2.py
@@ -1,30 +1,35 @@
+'''
+Created on 02.12.2016
+
 @author: weichslgartner
 '''
-import re
 
-keypad = [[1,2,3],[4,5,6],[7,8,9]]
+keypad = [['0','0','1','0','0'],
+          ['0','2','3','4','0'],
+          ['5','6','7','8','9'],
+          ['0','A','B','C','0'],
+          ['0','0','D','0','0']]
 
 
     
 
 with open('input2.dat') as file:
-    x=1
-    y=1
+    x=0
+    y=2
     for line in file:
-        print ("{0} {1}".format(x,y))
+       
         for char in line:
             if char == 'U':
-                if y > 0:
+                if y > 0 and keypad[y-1][x] != '0' :
                     y-=1
             elif char == 'L':
-                if x > 0:
+                if x > 0 and keypad[y][x-1] != '0':
                     x-=1
             elif char == 'R':
-                if x < 2:
+                if x < 4 and keypad[y][x+1] != '0':
                     x+=1
             elif char == 'D':          
-                if y < 2:
+                if y < 4 and keypad[y+1][x] != '0':
                     y+=1  
-            print(keypad[x][y], end="")
-        print(' ', end="")                
-        print(keypad[x][y])    
+      
+        print ("{2}".format(x,y,keypad[y][x]), end ="")
