d6ec1ffe60a7de195b996755a8259b078b1f39c9
Andreas Weichslgartner <andreas.weichslgartner@fau.de>
2016-12-09 16:04:01 +0100

    day9

--- src/Day9.py
+++ src/Day9.py
@@ -13,22 +13,25 @@ def calculateLength(line):
     if '(' not in line:
         return len(line)
     result =''
-    for i,char in enumerate(line):
+    i=0
+    while i < len(line):
         numbers = [1,1]
         if line[i] == '(':
             bracket=''
-            while line[i] != '(':
+            while line[i] != ')':
                 bracket += line[i]
                 i+=1
+            i+=1
             print(bracket)
             numbers = list(map(int,re.findall('\d+', bracket))) 
         for n in range(numbers[1]):
             for m in range(numbers[0]):
-                result+=line[i+m]
+                i+=m
+                result+=line[i]
     print(result)
     return calculateLength(result)
 
 #print(padArray[1][1])
 with open('input9.dat') as file:
     for line in file:
-        calculateLength(line)
+        print(calculateLength(line))
